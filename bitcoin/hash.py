c8f522f79f00addd515f40d91ddd5ebcd2c764ce
Hayawardh Vijayakumar <hayawardh@gmail.com>
2014-11-17 00:04:18 -0800

    add markle tree root hash calculation

--- bitcoin/hash.py
+++ bitcoin/hash.py
@@ -9,7 +9,7 @@ import json
 import hashlib
 import binascii
 
-def str_byte_rev(s):
+def swap_byte_order(s):
 	t = bytearray(len(s))
 	t[1::2], t[::2] = s[::2], s[1::2]
 	return str(t[::-1])
@@ -31,10 +31,46 @@ class BTCBlock:
 		self.time = h["time"]
 		self.bits = h["bits"]
 		self.nonce = h["nonce"]
+		self.tx_hashes = [t["hash"] for t in h["tx"]]
 		self.vals = [self.ver, self.prev_block, self.mrkl_root, self.time, self.bits, self.nonce]
 
 
-	def hash(self):
+	def mrkl_hash(self):
+		'''
+		the root hash of the merkle tree is calculated by:
+			(1) duplicating last transaction if odd number of transactions
+			(2) reversing bytes
+			(3) calculating parent hash = concatenated hash of pairs 
+			(4) recursively calculating (1) - (3) until we are left with root
+		reference: https://en.bitcoin.it/wiki/Protocol_specification#Merkle_Trees
+		'''
+		children = list(self.tx_hashes)
+		children = [str(c) for c in children]
+		while len(children) != 1:
+			parents = []
+			if len(children) % 2 != 0:
+				children.append(children[-1])
+			children = [swap_byte_order(c) for c in children]
+			for i in range(0, len(children), 2):
+				c = children[i] + children[i+1]
+				parents.append(self.coin_hash(c))
+			children = parents
+
+		return children[0]
+
+	def coin_hash(self, s):
+		'''
+		@s: string to hash
+		'''
+		# (4) : convert string to int
+		int_to_hash = binascii.a2b_hex(s)
+
+		# (5) : take sha256(sha256)
+		hsh = binascii.b2a_hex(hashlib.sha256(hashlib.sha256(int_to_hash).digest()).digest())
+
+		return swap_byte_order(hsh)
+		
+	def block_hash(self):
 		''' 
 		the hash of a block is calculated by:
 			(1) converting each value in (1) to hex string if required (without 0x, with leading 0s to fill size)
@@ -53,7 +89,7 @@ class BTCBlock:
 		# (2) : reverse order of bytes for each value 
 		rev_hex_vals = []
 		for s in hex_vals:
-			rev_hex_vals.append(str_byte_rev(s))
+			rev_hex_vals.append(swap_byte_order(s))
 
 		# (3) : concatenate 
 		str_to_hash = ''.join(rev_hex_vals)
@@ -64,7 +100,8 @@ class BTCBlock:
 		# (5) : take sha256(sha256)
 		hsh = binascii.b2a_hex(hashlib.sha256(hashlib.sha256(int_to_hash).digest()).digest())
 
-		return str_byte_rev(hsh)
+		return swap_byte_order(hsh)
 
 b = BTCBlock('125552.block')
-print 'hash: {0}'.format(b.hash())
+print 'mrkl root hash: {0}'.format(b.mrkl_hash())
+print 'block hash: {0}'.format(b.block_hash())
